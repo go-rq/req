4b33c631684fc41691a8a9270240d68a3d304cc0
bep <beneporter@gmail.com>
2023-11-26 14:37:01 -0800

    feat: add CLI implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,29 @@
 module github.com/go-rq/req
 
 go 1.21.3
+
+require (
+	github.com/alecthomas/chroma v0.10.0
+	github.com/atotto/clipboard v0.1.4
+	github.com/gdamore/tcell/v2 v2.6.0
+	github.com/go-rq/rq v0.1.0
+	github.com/rivo/tview v0.0.0-20231126152417-33a1d271f2b6
+	github.com/sahilm/fuzzy v0.1.0
+	github.com/samber/lo v1.38.1
+)
+
+require (
+	github.com/dlclark/regexp2 v1.7.0 // indirect
+	github.com/dop251/goja v0.0.0-20231027120936-b396bb4c349d // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/google/pprof v0.0.0-20230207041349-798e818bf904 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"context"
+	"os"
+
+	"github.com/go-rq/req/internal/tui"
+	"github.com/go-rq/rq"
+	"github.com/rivo/tview"
+)
+
 func main() {
+	app := tview.NewApplication()
+	ctx := context.Background()
+	path := "."
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+	fileSelectView := tui.NewFileSelectView(path)
+	fileSelectView.SetCallback(selectFile(ctx, app, fileSelectView))
+	fileSelectView.Mount(app)
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func selectFile(ctx context.Context, app *tview.Application, prevView tui.View) func(string) {
+	return func(path string) {
+		reqs, err := rq.ParseFromFile(path)
+		if err != nil {
+			panic(err)
+		}
+		rv := tui.NewRequestSelectView(app, reqs, prevView)
+		rv.SetCallback(selectRequest(ctx, app, rv))
+		rv.Mount(app)
+	}
+}
+
+func selectRequest(ctx context.Context, app *tview.Application, prevView tui.View) func(request rq.Request) {
+	return func(request rq.Request) {
+		rv := tui.NewRequestView(ctx, app, request, prevView)
+		rv.Mount(app)
+	}
 }
