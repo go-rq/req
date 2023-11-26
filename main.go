14ef8725015f2e68d7c108766ede17ecd020600d
bep <beneporter@gmail.com>
2023-11-26 15:32:16 -0800

    feat: add support for specifying .env files from the -e path/to/.env flag

--- main.go
+++ main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"flag"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/go-rq/req/internal/tui"
 	"github.com/go-rq/rq"
 	"github.com/rivo/tview"
 )
 
+var envFilePath string
+
+func init() {
+	initEnvFileFlags()
+}
+
 func main() {
+	flag.Parse()
 	app := tview.NewApplication()
 	ctx := context.Background()
+	if envFilePath != "" {
+		env, err := loadEnvFile(envFilePath)
+		if err != nil {
+			panic(err)
+		}
+		ctx = rq.WithEnvironment(ctx, env)
+	}
 	path := "."
-	if len(os.Args) > 1 {
-		path = os.Args[1]
+	if flag.NArg() > 1 {
+		path = flag.Arg(0)
 	}
 	fileSelectView := tui.NewFileSelectView(path)
 	fileSelectView.SetCallback(selectFile(ctx, app, fileSelectView))
@@ -41,4 +59,31 @@ func selectRequest(ctx context.Context, app *tview.Application, prevView tui.Vie
 		rv := tui.NewRequestView(ctx, app, request, prevView)
 		rv.Mount(app)
 	}
+}
+
+func initEnvFileFlags() {
+	const usage = "path to .env file"
+	flag.StringVar(&envFilePath, "env", "", usage)
+	flag.StringVar(&envFilePath, "e", "", usage+" (shorthand)")
+}
+
+func loadEnvFile(path string) (map[string]string, error) {
+	env := map[string]string{}
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(strings.TrimSpace(line), "#") {
+			continue
+		}
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid line: %s", line)
+		}
+		env[parts[0]] = parts[1]
+	}
+	return env, nil
 }
